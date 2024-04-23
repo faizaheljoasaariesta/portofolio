00e375a52ea47991b771e1eb01529c004103b5b7
Faizahel <macbookpro@FaizahelJoasa.local>
2024-04-23 18:12:46 +0700

    fixed planet hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-260px] sm:top-[-395px] left-0 z-[0] w-full h-full object-cover"
+        className="rotate-180 absolute top-[-300px] sm:top-[-395px] left-0 z-[0] w-full h-full object-cover"
       >
         <source src={blackHole} type="video/webm"/>
       </video>
